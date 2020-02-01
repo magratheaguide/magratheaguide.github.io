3cc55a20170e5a890296ada6a31738ba20201a60
daine <veralidaine.thewildmage@gmail.com>
2020-01-31 22:43:12 -0700

    change extensions

--- discord-bot-yagpdb/answer.go
+++
@@ -1,9 +0,0 @@
-{{$id := (index .CmdArgs 0)}}
-{{$answer := (joinStr " " (slice .CmdArgs 1))}}
-{{$color := 16776960}}
-{{$questionMsg := getMessage nil $id}}
-{{$questionEmbed := (index $questionMsg.Embeds 0)}}
-{{$question := $questionEmbed.Title}}
-{{$footer := $questionEmbed.Footer}}
-{{editMessage nil $id (cembed "title" $question "color" $color "description" $answer "footer" $footer)}}
-{{deleteAllMessageReactions nil $id}}
--- discord-bot-yagpdb/question.go
+++
@@ -1,6 +0,0 @@
-{{$question := (joinStr " " .CmdArgs)}}
-{{$color := 16711680}}
-{{$footerText := "Message ID:"}}
-{{ $embed := cembed "title" $question "color" $color "footer" (sdict "text" $footerText)}}
-{{$id := sendMessageRetID nil $embed }}
-{{editMessage nil $id (cembed "title" $question "color" $color "footer" (sdict "text" (joinStr " " $footerText $id)))}}
