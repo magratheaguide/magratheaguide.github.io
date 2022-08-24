476977875211690824101076181c66f9d713766e
thewildmage <cecilia@hey.com>
2022-08-23 20:49:21 -0600

    Specify templating language

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,12 @@ module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("source/assets");
 
 	return {
+		dataTemplateEngine: "njk",
 		dir: {
 			input: "source",
 			output: "_distribution",
 		},
+		htmlTemplateEngine: "njk",
+		markdownTemplateEngine: "njk",
 	};
 };
