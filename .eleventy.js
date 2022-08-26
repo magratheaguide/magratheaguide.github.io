ff569bd5dae3da0530740f02e17e33871ed3f77c
thewildmage <cecilia@hey.com>
2022-08-25 18:53:30 -0600

    Add primary navigation
    Note the switch to JSON front matter instead of YAML. This is because YAML strictly disallows tabs for indentation

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
 	eleventyConfig.addPassthroughCopy("source/_assets");
 
 	return {
