40bcd9230ee24e19f7f475f3a3808f94fa91c69b
thewildmage <cecilia@hey.com>
2022-09-01 17:05:27 -0600

    Move tags to entry footer, add filter stringifyList

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,17 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addFilter("excludeOne", (array, exclusion) => {
-		let result = array.filter((item) => item != exclusion);
-		return result;
+		return array.filter((item) => item != exclusion);
+	});
+
+	eleventyConfig.addFilter("stringifyList", (array) => {
+		// If any of the elements already contains a comma...
+		if (array.join(" ").includes(",")) {
+			// ...then separate elements with semicolons
+			return array.join("; ");
+		} else {
+			return array.join(", ");
+		}
 	});
 
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
