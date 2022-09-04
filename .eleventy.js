2e3c742620e78b71c6f0ea8e29fb0c08df959b09
thewildmage <cecilia@hey.com>
2022-09-04 10:40:35 -0600

    Add collections structure and item metadata (#31)
    - Configuration changes:
      - Eleventy:
        - Run Prettier on `_distribution` after each build so that the deployed code is well-formatted and easier to read
        - Add filter to exclude one specific item from array
        - Add shortcode to return inline linked list of tags
      - Prettier:
        - Use `tabWidth: 2` for YAML indentation (instead of 4)
      - NPM:
        - Remove unnecessary content from `package.json`. I'm not planning on publishing a package, so most of the metadata isn't needed. Rather than maintain redundant data that can fall out of sync, just remove it
        - Run `clean` script on `build` as well as `start`
    - Content structure changes:
        - Add collection layout for use with Tools and Themes pages
        - Add "Learn GitHub" partial to improve reuse
        - Capitalize tags
        - Set `layouts/base.html` as default layout for all pages
        - Add "Tagged" page to show all entries with specific tag
        - Add "Tags" page to list all tags
        - Enable generation of pages for individual Tools and Themes
    - Content changes:
        - Add resources' metadata

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,47 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const prettier = require("prettier");
 
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+	// Organization is alphabetical when order doesn't matter
+
+	eleventyConfig.addFilter("excludeOne", (array, exclusion) => {
+		return array.filter((item) => item != exclusion);
+	});
 
 	eleventyConfig.addPassthroughCopy("source/_assets");
+
+	eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+	eleventyConfig.addShortcode("linkedTagsList", (tags) => {
+		const separator = tags.join(" ").includes(",") ? "; " : ", ";
+		let linkedTags = [];
+
+		tags.forEach((tag) => {
+			linkedTags.push(
+				`<a href="/tags/${eleventyConfig.getFilter("slugify")(
+					tag
+				)}">${tag}</a>`
+			);
+		});
+
+		return linkedTags.join(separator);
+	});
+
+	eleventyConfig.addTransform("prettier", function (content) {
+		const path = this.outputPath;
+
+		if (!path) {
+			return content;
+		}
+
+		async function prettierFormat(content, path) {
+			let config = await prettier.resolveConfig(path);
+			config.filepath = path;
+			return prettier.format(content, config);
+		}
+
+		return prettierFormat(content, path);
+	});
 
 	return {
 		dataTemplateEngine: "njk",
--- source/tools/cybertron.md
+++ source/tools/cybertron.md
@@ -1,7 +1,12 @@
 ---json
 {
+	"links": {
+		"Cybertron on GitHub": "https://github.com/magratheaguide/cybertron"
+	},
+	"subtitle": "CLI theme compiler",
+	"tags": "Jcink",
 	"title": "Cybertron"
 }
 ---
 
-TODO: Add info
+A command-line interface (CLI) tool to transform a directory full of theme files into a single, Jcink-importable XML file.
