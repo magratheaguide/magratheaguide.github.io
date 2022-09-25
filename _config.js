e77e6bdf8ad77ac4902f38c6da7c3df8198262fd
thewildmage <cecilia@hey.com>
2022-09-25 09:25:57 -0600

    Update linkedHeading helper

--- _config.js
+++ _config.js
@@ -1,10 +1,33 @@
 import lume from "lume/mod.ts";
+import { createSlugifier } from "lume/plugins/slugify_urls.ts";
 
 const site = lume({
 	dest: "./_distribution",
 	location: new URL("https://magrathea.guide"),
 	src: "./source",
 });
+
+site.helper(
+	"linkedHeading",
+	(level, heading, id = null) => {
+		/* Informed by:
+	https://amberwilson.co.uk/blog/are-your-anchor-links-accessible/
+	and
+	https://www.leereamsnyder.com/blog/making-headings-with-links-show-up-in-safari-reader
+	*/
+
+		if (!id) {
+			id = createSlugifier()(heading);
+		}
+
+		return `<h${level} id="${id}">
+	<a href="#${id}">
+		<span>${heading}</span>
+	</a>
+</h${level}>`;
+	},
+	{ type: "tag" }
+);
 
 site.loadAssets([".css"]);
 
