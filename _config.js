67afe0771e882b1863e9205c946732c1d42f2d3b
thewildmage <cecilia@hey.com>
2022-09-29 15:23:57 -0600

    Run js-beautify on distribution CSS and HTML

--- _config.js
+++ _config.js
@@ -1,5 +1,7 @@
 import lume from "lume/mod.ts";
 import { createSlugifier } from "lume/plugins/slugify_urls.ts";
+import beautify from "jsBeautify";
+
 import anchorHeadings from "./plugins/anchor_headings.ts";
 
 const site = lume({
@@ -9,11 +11,17 @@ const site = lume({
 });
 
 site.filter("slugify", (text) => createSlugifier()(text))
-	.loadAssets([".css"])
 	.use(
 		anchorHeadings({
 			queryString: ":is(h2, h3)[data-anchor-heading]",
 		})
-	);
+	)
+	.process([".html"], function (page) {
+		page.content = beautify.html(page.content);
+	})
+	.loadAssets([".css"])
+	.process([".css"], function (page) {
+		page.content = beautify.css(page.content);
+	});
 
 export default site;
